88f1faaa3ac7a19299e9ed7323b2962383f69e94
syafrud <115003591+syafrud@users.noreply.github.com>
2024-03-21 11:18:55 +0700

    login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,11 +1,67 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axiosClient from "../axios.js";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+
 export default function Login() {
+  const { setCurrentUser, setUserToken } = useStateContext();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState({ __html: "" });
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    setError({ __html: "" });
+
+    axiosClient
+      .post("/login", {
+        email,
+        password,
+      })
+      .then(({ data }) => {
+        setCurrentUser(data.user);
+        setUserToken(data.token);
+      })
+      .catch((error) => {
+        if (error.response) {
+          const finalErrors = Object.values(error.response.data.errors).reduce(
+            (accum, next) => [...accum, ...next],
+            []
+          );
+          setError({ __html: finalErrors.join("<br>") });
+        }
+        console.error(error);
+      });
+  };
+
   return (
     <>
       <h2 className="mt-10 text-center text-3xl  font-bold leading-9 tracking-tight text-gray-900">
         Sign in to your account
       </h2>
+      <p className="mt-2 text-center text-sm text-gray-600">
+        Or{" "}
+        <Link
+          to="/signup"
+          className="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          signup for free
+        </Link>
+      </p>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        {error.__html && (
+          <div
+            className="bg-red-500 rounded py-2 px-3 text-white"
+            dangerouslySetInnerHTML={error}
+          ></div>
+        )}
+        <br />
+        <form
+          onSubmit={onSubmit}
+          className="space-y-6"
+          action="#"
+          method="POST"
+        >
           <div>
             <label
               htmlFor="email"
@@ -20,6 +76,8 @@ export default function Login() {
                 type="email"
                 autoComplete="email"
                 required
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
                 className="block w-full lg:text-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
@@ -33,14 +91,6 @@ export default function Login() {
               >
                 Password
               </label>
-              <div className="text-lg">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -49,6 +99,8 @@ export default function Login() {
                 type="password"
                 autoComplete="current-password"
                 required
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
                 className="block w-full lg:text-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
               />
             </div>
