2795abcc9ea563be91354e7f2e3330896424d820
syafrud <115003591+syafrud@users.noreply.github.com>
2024-03-21 11:06:24 +0700

    Signup

--- src/contexts/ContextProvider.jsx
+++ src/contexts/ContextProvider.jsx
@@ -5,7 +5,7 @@ const StateContext = createContext({
   userToken: null,
   surveys: [],
   setCurrentUser: () => {},
-  setUSerToken: () => {},
+  setUserToken: () => {},
 });
 
 const tmpSurveys = [
@@ -187,14 +187,20 @@ const tmpSurveys = [
 ];
 
 export const ContextProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState({
-    name: "Tom Cook",
-    email: "tom@example.com",
-    imageUrl:
-      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
-  });
-  const [userToken, setUSerToken] = useState("123");
+  const [currentUser, setCurrentUser] = useState({});
+  const [userToken, _setUserToken] = useState(
+    localStorage.getItem("TOKEN") || ""
+  );
   const [surveys, setSurveys] = useState(tmpSurveys);
+
+  const setUserToken = (token) => {
+    if (token) {
+      localStorage.setItem("TOKEN", token);
+    } else {
+      localStorage.removeItem("TOKEN");
+    }
+    _setUserToken(token);
+  };
 
   return (
     <StateContext.Provider
@@ -202,7 +208,7 @@ export const ContextProvider = ({ children }) => {
         currentUser,
         setCurrentUser,
         userToken,
-        setUSerToken,
+        setUserToken,
         surveys,
       }}
     >
--- src/views/Signup.jsx
+++ src/views/Signup.jsx
@@ -1,11 +1,93 @@
+import { Link } from "react-router-dom";
+import { useState } from "react";
+import axiosClient from "../axios.js";
+import { useStateContext } from "../contexts/ContextProvider.jsx";
+
 export default function Signup() {
+  const { setCurrentUser, setUserToken } = useStateContext();
+  const [fullName, setFullName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  const [error, setError] = useState({ __html: "" });
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    setError({ __html: "" });
+
+    axiosClient
+      .post("/signup", {
+        name: fullName,
+        email,
+        password,
+        password_confirmation: passwordConfirmation,
+      })
+      .then(({ data }) => {
+        setCurrentUser(data.user);
+        setUserToken(data.token);
+      })
+      .catch((error) => {
+        if (error.response) {
+          const finalErrors = Object.values(error.response.data.errors).reduce(
+            (accum, next) => [...accum, ...next],
+            []
+          );
+          console.log(finalErrors);
+          setError({ __html: finalErrors.join("<br>") });
+        }
+        console.error(error);
+      });
+  };
+
   return (
     <>
       <h2 className="mt-10 text-center text-3xl  font-bold leading-9 tracking-tight text-gray-900">
-        Sign up for free
+        Signup for free
       </h2>
+      <p className="mt-2 text-center text-sm text-gray-600">
+        Or{" "}
+        <Link
+          to="/login"
+          className="font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          Login with your account
+        </Link>
+      </p>
+
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
+        {error.__html && (
+          <div
+            className="bg-red-500 rounded py-2 px-3 text-white"
+            dangerouslySetInnerHTML={error}
+          ></div>
+        )}
+        <br />
+        <form
+          onSubmit={onSubmit}
+          className="space-y-6"
+          action="#"
+          method="POST"
+        >
+          <div>
+            <label
+              htmlFor="full-name"
+              className="block text-lg font-medium leading-6 text-gray-900"
+            >
+              Full Name
+            </label>
+            <div className="mt-2">
+              <input
+                id="full-name"
+                name="name"
+                type="text"
+                required
+                value={fullName}
+                onChange={(ev) => setFullName(ev.target.value)}
+                className="block w-full lg:text-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Full Name"
+              />
+            </div>
+          </div>
           <div>
             <label
               htmlFor="email"
@@ -20,11 +102,13 @@ export default function Signup() {
                 type="email"
                 autoComplete="email"
                 required
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
                 className="block w-full lg:text-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Email address"
               />
             </div>
           </div>
-
           <div>
             <div className="flex items-center justify-between">
               <label
@@ -33,14 +117,6 @@ export default function Signup() {
               >
                 Password
               </label>
-              <div className="text-lg">
-                <a
-                  href="#"
-                  className="font-semibold text-indigo-600 hover:text-indigo-500"
-                >
-                  Forgot password?
-                </a>
-              </div>
             </div>
             <div className="mt-2">
               <input
@@ -49,11 +125,35 @@ export default function Signup() {
                 type="password"
                 autoComplete="current-password"
                 required
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
                 className="block w-full lg:text-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Password"
+              />
+            </div>
+          </div>{" "}
+          <div>
+            <div className="flex items-center justify-between">
+              <label
+                htmlFor="password-confirmation"
+                className="block text-lg font-medium leading-6 text-gray-900"
+              >
+                Password Conformation
+              </label>
+            </div>
+            <div className="mt-2">
+              <input
+                id="password-confirmation"
+                name="password_confirmation"
+                type="password"
+                required
+                value={passwordConfirmation}
+                onChange={(ev) => setPasswordConfirmation(ev.target.value)}
+                className="block w-full lg:text-lg rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                placeholder="Password Confirmation"
               />
             </div>
           </div>
-
           <div>
             <button
               type="submit"
